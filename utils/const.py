b64d6c858fba8db0be62b09c81b75aec7fd1e41a
cauaosp <cauaosp20@gmail.com>
2026-07-09 00:14:39 -0300

    adicionado padronização nos headers de todas as requisições

--- utils/const.py
+++ utils/const.py
@@ -1,3 +1,11 @@
+DEFAULT_HEADERS = {
+    "User-Agent": (
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) "
+        "AppleWebKit/537.36 (KHTML, like Gecko) "
+        "Chrome/138.0.0.0 Safari/537.36"
+    )
+}
+
 JORNAIS_MAP = {
     "opovo": {
         "url": "https://www.opovo.com.br/index.php",
@@ -7,37 +15,35 @@ JORNAIS_MAP = {
             "site_arvor": "Vida&Arte",
             "limit": 30,
         },
-        "headers": {"User-Agent": "Mozilla/5.0"},
+        "headers": DEFAULT_HEADERS,
     },
     "dn": {
         "url": "https://diariodonordeste.verdesmares.com.br/ceara",
-        "headers": {"User-Agent": "Mozilla/5.0"},
+        "headers": DEFAULT_HEADERS,
     },
     "oestadoce": {
         "url": "https://oestadoce.com.br/wp-json/wp/v2/posts",
         "params": {"categories": "8", "page": "1", "per_page": "30"},
-        "headers": {"User-Agent": "Mozilla/5.0"},
+        "headers": DEFAULT_HEADERS,
     },
     "verdemares": {
         "url": "https://g1.globo.com/rss/g1/ce/ceara",
-        "headers": {"User-Agent": "Mozilla/5.0"},
+        "headers": DEFAULT_HEADERS,
     },
     "cearaagora": {
         "url": "https://cearaagora.com.br/wp-json/wp/v2/posts",
         "params": {"page": "1", "per_page": "30"},
-        "headers": {
-            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36"
-        },
+        "headers": DEFAULT_HEADERS,
     },
     "tce": {
         "url": "https://www.tce.ce.gov.br/comunicacao/noticias",
         "params": {"format": "feed", "type": "atom", "start": "0"},
-        "headers": {"User-Agent": "Mozilla/5.0"},
+        "headers": DEFAULT_HEADERS,
     },
     "terra_da_luz": {
         "url": "https://portalterradaluz.com.br/wp-json/wp/v2/posts",
         "params": {"page": "1", "per_page": "30"},
-        "headers": {"User-Agent": "Mozilla/5.0"},
+        "headers": DEFAULT_HEADERS,
     },
     "jangadeiro": {
         "url": "https://www.jangadeiro.com.br/wp-json/wp/v2/posts",
@@ -46,6 +52,6 @@ JORNAIS_MAP = {
             "per_page": "30",
             "page": "1",
         },
-        "headers": {"User-Agent": "Mozilla/5.0"},
+        "headers": DEFAULT_HEADERS,
     },
 }
