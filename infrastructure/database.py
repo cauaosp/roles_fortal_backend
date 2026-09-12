a6d047e2e9684e6d8c1b6b202b4aa863d11f4e79
cauaosp <cauaosp20@gmail.com>
2026-09-11 21:54:43 -0300

    fix: ajustado engine do psycopg

--- infrastructure/database.py
+++ infrastructure/database.py
@@ -9,12 +9,14 @@ from sqlalchemy.orm import DeclarativeBase
 from sqlalchemy.pool import NullPool
 
 DB_URL = os.environ.get("DB_URL")
+
 if not DB_URL:
     raise ValueError("❌ DB_URL não configurada no .env")
 
 engine = create_engine(
     DB_URL.replace("postgresql://", "postgresql+psycopg://"),
     poolclass=NullPool,
+    connect_args={"prepare_threshold": None}
 )
 
 
